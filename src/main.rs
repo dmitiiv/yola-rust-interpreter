24c291ab37928f5a17d4e6fd0c838e0d24e91444
dm <davarlamov810@gmail.com>
2024-08-18 20:44:53 +0300

    feat: add read file functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::{env, fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() > 1 {
+        println!("args len {:?}", &args)
+    } else if args.len() == 1 {
+        println!("args len {:?}", &args);
+        run_file(&args[0]);
+    } else {
+        println!("args len {:?}", &args)
+    }
+}
+
+fn run_file(path: &str) {
+    let mut reader = SourceReader::new();
+
+    reader
+        .read_sources(path)
+        .expect("Error during read sources");
+
+    let _source = reader.get_sources();
+}
+
+struct SourceReader {
+    buffer: Vec<u8>,
+}
+
+impl SourceReader {
+    fn new() -> SourceReader {
+        SourceReader { buffer: vec![] }
+    }
+
+    fn read_sources(&mut self, path: &str) -> Result<(), String> {
+        let mut file = File::open(path).unwrap();
+        self.buffer.clear();
+
+        file.read_to_end(&mut self.buffer);
+
+        Ok(())
+    }
+
+    fn get_sources(&self) -> &Vec<u8> {
+        &self.buffer
+    }
 }
