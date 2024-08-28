68a647c86e2f7fd8913fa593e7501da6c5a2f13f
dm <davarlamov810@gmail.com>
2024-08-28 23:08:51 +0300

    feat: add first test

--- src/report.rs
+++ src/report.rs
@@ -3,11 +3,32 @@ use std::process;
 struct Report {}
 
 impl Report {
-    pub fn error(line: i32, err: String, message: String, code: Option<i32>) {
+    pub fn new() -> Report {
+        Report {}
+    }
+
+    pub fn error(&self, code: Option<i32>, err: String, line: i32, message: String) {
         let code = code.unwrap_or(1);
 
         println!("[ Line {}] Error {}: {}", line, err, message);
 
         process::exit(code);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn report_error() {
+        let report = Report::new();
+        let line = 1;
+        let mess = String::from("message");
+        let error = String::from("error");
+
+        let code = Some(2);
+
+        assert_eq!((), report.error(code, error, line, mess));
     }
 }
