727a6f03410c4a336785b26569e18f4a0c5fbd0f
dm <davarlamov810@gmail.com>
2024-08-29 22:33:42 +0300

    feat: add yola module

---
+++ src/lang/yola.rs
@@ -0,0 +1,57 @@
+use crate::{scanner::Scanner, source_reader::SourceReader};
+use std::io;
+
+pub struct Yola {
+    had_error: bool,
+}
+
+impl Yola {
+    pub fn new() -> Yola {
+        Yola { had_error: false }
+    }
+
+    pub fn run_file(&self, path: &str) {
+        let mut reader = SourceReader::new();
+
+        if let Err(err) = reader.read_sources(path) {
+            eprintln!("Error reading sources: {:?}", err);
+            return;
+        }
+
+        match reader.get_sources() {
+            Ok(_source) => Yola::run(_source),
+            Err(err) => println!("Error - {:?}", err),
+        }
+    }
+
+    pub fn run_prompt(&self) {
+        loop {
+            let mut input = String::new();
+
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Error parsing promt");
+
+            let trimmed_input = input.trim();
+
+            if trimmed_input.eq_ignore_ascii_case("exit") {
+                break;
+            }
+
+            Yola::run(trimmed_input.to_owned())
+        }
+    }
+
+    pub fn run(source: String) {
+        let scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens();
+
+        for token in tokens {
+            println!("Token - {:?}", token);
+        }
+    }
+
+    pub fn set_error(&mut self) {
+        self.had_error = true;
+    }
+}
