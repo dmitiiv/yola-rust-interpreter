1d8a965a9f9c3cc8f1adcba5c10dc0faed88f8ec
davarlamov <davarlamov@avito.ru>
2024-10-16 23:49:16 +0300

    feat: change ast naming

--- src/ast/binary.rs
+++ src/ast/binary.rs
@@ -3,15 +3,15 @@ use crate::lexemes::token::Token;
 use super::expr::Expr;
 
 #[derive(Debug)]
-pub struct BinaryExpr {
+pub struct Binary {
     left: Box<Expr>,
     operator: Box<Token>,
     right: Box<Expr>,
 }
 
-impl BinaryExpr {
-    pub fn new(&self, left: Expr, operator: Token, right: Expr) -> BinaryExpr {
-        BinaryExpr {
+impl Binary {
+    pub fn new(&self, left: Expr, operator: Token, right: Expr) -> Binary {
+        Binary {
             left: Box::new(left),
             operator: Box::new(operator),
             right: Box::new(right),
