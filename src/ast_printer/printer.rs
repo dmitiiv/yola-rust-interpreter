6cd93e993780fd32bdb7e84a4230d350ebfe266b
dm <davarlamov810@gmail.com>
2024-10-22 20:22:07 +0300

    feat: add ast printer

--- src/ast_printer/printer.rs
+++ src/ast_printer/printer.rs
@@ -1,9 +1,31 @@
-use crate::ast::expr::Expr;
+use crate::ast::expr::{Expr, Expressions, Visitor};
 
-pub struct Printer {}
+pub struct AstPrinter {}
 
-impl Printer {
-    fn print(&self, expr: Expr) {
-        expr.visitor(&self);
+impl AstPrinter {
+    fn print(&mut self, expr: Box<&dyn Expr<Parent = Expressions>>) {
+        expr.accept(self);
+    }
+}
+
+// impl Expr for AstPrinter {
+//     type Parent = Self;
+
+//     fn accept(&mut self, visitor: &mut dyn Visitor) {
+//         todo!()
+//     }
+// }
+
+impl Visitor for AstPrinter {
+    fn visit_binary(&mut self, expr: &crate::ast::binary::Binary) {
+        todo!()
+    }
+
+    fn visit_unary(&mut self, expr: &crate::ast::unary::Unary) {
+        todo!()
+    }
+
+    fn visit_group(&mut self, expr: &crate::ast::group::Group) {
+        todo!()
     }
 }
